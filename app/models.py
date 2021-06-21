8efad8822234c4987a8ed59cc9053a77ed079ea5
YK-K <chita0731@khu.ac.kr>
2021-06-22 04:54:00 +0900

    modify strategy model

--- app/models.py
+++ app/models.py
@@ -5,13 +5,17 @@ from django.utils import timezone
 class Strategy(models.Model):
 	raceType = models.TextChoices('Race', 'Terran Zerg Protoss') # 테 프 저
 	
-	title = models.CharField(max_length=200) # 
-	playerRace = models.CharField(blank=False, choices=raceType.choices, max_length=10) #
-	enemyRace = models.CharField(blank=False, choices=raceType.choices, max_length=10) #
-	text = models.TextField() #
-	password = models.CharField(max_length=10) #
-	created_date = models.DateTimeField(default=timezone.now) #
-	modified_date = models.DateTimeField(blank=True, null=True) #
+	title = models.CharField(max_length=200) # 전략명
+	playerRace = models.CharField(blank=False, choices=raceType.choices, max_length=10) # 플레이어 종족
+	enemyRace = models.CharField(blank=False, choices=raceType.choices, max_length=10) # 상대 종족
+	keyUnit = models.TextField() # 핵심 유닛
+	strategyType = models.TextField() # 전략 형태(타이밍, 올인, ...)
+	difficulty = models.TextChoices('difficulty', 'Easy Normal Hard') # 난이도
+	mapname = models.TextField() # 맵
+	text = models.TextField() # 전략 상세 내용
+	password = models.CharField(max_length=10) # 수정 비밀번호
+	created_date = models.DateTimeField(default=timezone.now) # 전략을 작성한 날짜
+	modified_date = models.DateTimeField(blank=True, null=True) # 전략을 수정한 날짜
 	
 	def create(self):
 		self.created_date = timezone.now()
--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Strategy
 
 # Create your views here.
 def index(request):
 	return render(request, 'app/index.html', {})
+	
+def strategy_search(request):
+	strategies = Strategy.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+	return render(request, 'app/search.html', {'strategies':strategies})
